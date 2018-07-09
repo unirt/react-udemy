1ceebe487f7507b5e45bca40f494aaaad4f892e7
unirt <lunirtc@gmail.com>
2018-07-09 19:29:44 +0900

    jsx finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 
-class App extends Component {
-  render() {
-    return (<div><h1 className="App-title">Hello world</h1></div>);
+const App = () => (<Counter />)
+
+class Counter extends Component  {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
   }
-}
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1})
+  }
+  render() {
+    return (
+    <React.Fragment>
+      <div>count: { this.state.count }</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinusButton}>-1</button>
+    </React.Fragment>
+  )
+  }
+};
 
 export default App;
