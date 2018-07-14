7c568effa88fa40ecd9529f1a04a3ac11312c185
unirt <lunirtc@gmail.com>
2018-07-15 03:40:46 +0900

    centred

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-
 import './index.css';
 import reducer from './reducers';
 import App from './components/App';
