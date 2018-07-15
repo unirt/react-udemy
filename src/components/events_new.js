a7d295ecc6bfc3c02dbd0c94e46f687bef83fc27
unirt <lunirtc@gmail.com>
2018-07-15 10:15:26 +0900

    implement Event new component

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,20 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm, defaultShouldValidate, formValues} from 'redux-form';
 import { Link } from 'react-router-dom';
 
-//import { postEvent } from '../actions';
+import { postEvent } from '../actions';
 
 
 class EventsNew extends Component  {
-  render() {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderfield(field) {
+    const { input, label, type, meta: { touched, error } } = field
     return (
-      <React.Fragment>
-        <div>foo</div>
-      </React.Fragment>
+      <form>
+       <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+       </div>
+      </form>
+    )
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values)
+    this.props.history.push('/')
+  }
+
+  render() {
+    const { handleSubmit } = this.props
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div><Field label="Title" name="title" type="text" component={this.renderfield}></Field></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderfield}></Field></div>
+
+        <div>
+          <input type="submit" value="Submit" disabled={false} />
+          <Link to="/" >Cancel</Link>
+        </div>
+      </form>
   )
   }
 };
 
-//const mapDispatchToProps = ({ postEvent })
+const validate = values => {
+  const errors = {}
 
-export default connect(null, null)(EventsNew)
+  if (!values.title) errors.title = "Enter a title, please."
+  if (!values.body) errors.body = "Enter a body, please."
+
+  return errors
+}
+const mapDispatchToProps = ({ postEvent })
+
+export default connect(null, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
