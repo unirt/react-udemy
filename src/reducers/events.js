46f0c6c9fa9becaa54980211d0b3e58f799e1e20
unirt <lunirtc@gmail.com>
2018-07-23 05:12:22 +0900

    implement delete func

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
-import { READ_EVENTS } from '../actions';
+import { READ_EVENTS, DELETE_EVENT, } from '../actions';
 
 export default (events = {}, action) =>  {
     switch (action.type) {
         case READ_EVENTS:
         //console.log(action.response.then((data)=>{console.log(data.data)}))
             return _.mapKeys(action.response.data, 'id')
+        case DELETE_EVENT:
+            console.log(action.id)
+            delete events[action.id]
+            return { ...events }
         default:
             return events
     }
